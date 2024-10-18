789ed8f9789f1e171401c7213f208b529acd4f84
predator4hack <chandankum2440@gmail.com>
2024-10-18 18:59:09 +0530

    Added logo and page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,33 @@
-import Image from 'next/image'
-import './globals.css'
-import Nav from './components/navbar'
-import Footer from './components/footer';
+import Image from "next/image";
+import "./globals.css";
+import Nav from "./components/navbar";
+import Footer from "./components/footer";
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
 } from "@/components/ui/carousel";
-import { Card ,CardContent} from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
-import Autoplay from "embla-carousel-autoplay"
-import ImageCarousel from './components/carousel';
+import Autoplay from "embla-carousel-autoplay";
+import ImageCarousel from "./components/carousel";
 
-export default function Home () {
-  return (
-   <>
-    {/* <div className='grid grid-cols-12 gap-2'>
+export default function Home() {
+    return (
+        <>
+            {/* <div className='grid grid-cols-12 gap-2'>
       <div></div>
      <div className='col-start-3 col-span-8'> <Nav/></div> 
       <div></div>
       <div className='col-start-2 col-end-12'> */}
-      <ImageCarousel/>
-    {/* </div>
+            <ImageCarousel />
+            {/* </div>
     <div className='col-start-2 col-end-12'></div>
     </div>
    <Footer/> */}
-  </>
-  );
+        </>
+    );
 }
