ae5ee8d967f6f8b9e0d0a3708c4bd1f9a1c0a5b3
Nitesh Gupta <niteskug@Niteshs-MacBook-Pro.local>
2024-10-27 19:25:16 +0530

    added manifest.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,32 @@ export default function RootLayout ({
     <html lang='en'>
       <Head>
         <link rel='icon' href='../icon.ico' type='image/svg+xml' />
+
+        <link rel='manifest' href='/manifest.json' />
+
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='application-name' content='Eraya Foods' />
+        <meta name='apple-mobile-web-app-title' content='Eraya Foods' />
+        <meta name='theme-color' content='#000000' />
+        <meta name='msapplication-navbutton-color' content='#000000' />
+        <meta
+          name='apple-mobile-web-app-status-bar-style'
+          content='black-translucent'
+        />
+        <meta name='msapplication-starturl' content='/' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, shrink-to-fit=no'
+        />
+
+        <link rel='icon' type='image/png' sizes='192x192' href='/icon.ico' />
+        <link
+          rel='apple-touch-icon'
+          type='image/png'
+          sizes='192x192'
+          href='/icon.ico'
+        />
       </Head>
       <body className='flex flex-col min-h-screen'>
         <Nav />
---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default function NotFound () {
+  return (
+    <div className='backgroundImage4'>
+      <div className='flex items-center justify-center h-screen bg-cover bg-center'>
+        <div className='bg-white bg-opacity-40 rounded-lg shadow-lg p-10 text-center'>
+          <Link href='/'>
+            <div className='px-6 py-3 bg-blue-600 opacity-80 text-white rounded-lg hover:bg-blue-700 transition duration-300'>
+              Return Home
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
