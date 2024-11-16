adfa499ee7b0a03ed500552ce883d250084b0430
Nitesh Gupta <niteskug@Niteshs-MacBook-Pro.local>
2024-11-16 19:42:16 +0530

    adding image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
     siteName: 'Eraya Foods',
     images: [
       {
-        url: 'https://erayafoods.com/eraya.svg',
+        url: 'https://erayafoods.com/eraya.jpeg',
         width: 1200,
         height: 630,
         alt: 'Eraya Foods Premium Foxnuts'
