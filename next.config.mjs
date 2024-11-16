9e3b5caf94c44a8529170e9b31bf1dde50943123
Nitesh Gupta <niteskug@Niteshs-MacBook-Pro.local>
2024-11-17 01:03:06 +0530

    CSP Header allowing internal libraries etc

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,19 @@ const nextConfig = withOptimizedImages({
           {
             key: 'X-Frame-Options',
             value: 'SAMEORIGIN'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://unpkg.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data:; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://www.google-analytics.com;"
           }
         ]
       }
