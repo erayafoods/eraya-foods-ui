1612b12ba977a801a4771607a2cc697d3f97c2aa
Nitesh Gupta <niteskug@Niteshs-MacBook-Pro.local>
2024-11-17 18:24:20 +0530

    sample jsonLD for products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -34,6 +34,23 @@ export const metadata: Metadata = {
   }
 }
 
+const jsonLd = {
+  '@context': 'http://schema.org',
+  '@graph': [
+    {
+      '@type': 'Product',
+      name: 'tshirt',
+      description: 'test copy 1.',
+      image: 'image.jpg'
+    },
+    {
+      '@type': 'Product',
+      name: 'tshirt 2',
+      description: 'test copy 2.',
+      image: 'image2.jpg'
+    }
+  ]
+}
 const inter = Inter({
   subsets: ['latin'],
   weight: '400'
@@ -44,188 +61,199 @@ const ibm = IBM_Plex_Serif({
 })
 const Products = () => {
   return (
-    <div className='max-w-screen-2xl content-center px-[2vw] pb-[6vh] bg-gray-100'>
-      {/* Foxnut Grades Section */}
-      <section className='py-16'>
-        <h1
-          className='text-4xl text-center font-extrabold text-gray-800 mb-12'
-          style={{ fontFamily: ibm.style.fontFamily }}
-        >
-          Foxnut Grades & Packaged Raw Makhana
-        </h1>
+    <>
+      <script
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <div className='max-w-screen-2xl content-center px-[2vw] pb-[6vh] bg-gray-100'>
+        {/* Foxnut Grades Section */}
+        <section className='py-16'>
+          <h1
+            className='text-4xl text-center font-extrabold text-gray-800 mb-12'
+            style={{ fontFamily: ibm.style.fontFamily }}
+          >
+            Foxnut Grades & Packaged Raw Makhana
+          </h1>
 
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto lg:px-[6vw]'>
-          {/* Row 1 - 3 Cards */}
-          <div className='bg-white rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 transition-transform'>
-            <picture>
-              <source srcSet='/gradeA.avif?webp' type='image/webp' />
-              <Image
-                src='/gradeA.avif'
-                alt='Grade B Foxnuts'
-                width={120}
-                height={200}
-                className='w-full h-48 object-cover'
-              />
-            </picture>
-            <div className='p-6' style={{ fontFamily: inter.style.fontFamily }}>
-              <h2 className='text-2xl font-bold text-gray-700 mb-4'>Grade A</h2>
-              <p className='text-gray-600'>
-                Top quality foxnuts, ideal for premium snack packaging.
-              </p>
-              <p className='text-gray-600 mt-4'>
-                Specification: Best Quality Graded, Round Shaped
-                <br />
-                <br />
-                Size: 13mm-16mm
-                <br />
-                Mandi: 4 sutta
-                <br />
-                Bag Size: 5kg/8kg/10kg
-              </p>
-            </div>
-          </div>
-
-          <div className='bg-white rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 transition-transform'>
-            <picture>
-              <source srcSet='/gradeB.avif?webp' type='image/webp' />
-              <Image
-                src='/gradeB.avif'
-                alt='Grade B Foxnuts'
-                width={120}
-                height={200}
-                className='w-full h-48 object-cover'
-              />
-            </picture>
-            <div className='p-6'>
-              <h2
-                className='text-2xl font-bold text-gray-700 mb-4'
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto lg:px-[6vw]'>
+            {/* Row 1 - 3 Cards */}
+            <div className='bg-white rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 transition-transform'>
+              <picture>
+                <source srcSet='/gradeA.avif?webp' type='image/webp' />
+                <Image
+                  src='/gradeA.avif'
+                  alt='Grade B Foxnuts'
+                  width={120}
+                  height={200}
+                  className='w-full h-48 object-cover'
+                />
+              </picture>
+              <div
+                className='p-6'
                 style={{ fontFamily: inter.style.fontFamily }}
               >
-                Grade B
-              </h2>
-              <p className='text-gray-600'>
-                Perfect for roasted or flavored foxnut production.
-              </p>
-              <p className='text-gray-600 mt-4'>
-                Specification: Best Quality Graded, Round Shaped
-                <br />
-                Ready to use, No Wastage
-                <br />
-                Size: 16mm -19 mm
-                <br />
-                Mandi: 5 sutta
-                <br />
-                Bag Size: 5kg/8kg/10kg
-              </p>
+                <h2 className='text-2xl font-bold text-gray-700 mb-4'>
+                  Grade A
+                </h2>
+                <p className='text-gray-600'>
+                  Top quality foxnuts, ideal for premium snack packaging.
+                </p>
+                <p className='text-gray-600 mt-4'>
+                  Specification: Best Quality Graded, Round Shaped
+                  <br />
+                  <br />
+                  Size: 13mm-16mm
+                  <br />
+                  Mandi: 4 sutta
+                  <br />
+                  Bag Size: 5kg/8kg/10kg
+                </p>
+              </div>
             </div>
-          </div>
 
-          <div className='bg-white rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 transition-transform'>
-            <picture>
-              <source srcSet='/gradeC.avif?webp' type='image/webp' />
-              <Image
-                src='/gradeC.avif'
-                alt='Grade C Foxnuts'
-                width={120}
-                height={200}
-                className='w-full h-48 object-cover'
-              />
-            </picture>
-            <div className='p-6'>
-              <h2
-                className='text-2xl font-bold text-gray-700 mb-4'
-                style={{ fontFamily: inter.style.fontFamily }}
-              >
-                Grade C
-              </h2>
-              <p className='text-gray-600'>
-                Used in culinary recipes like curries, gravies, and snacks.
-              </p>
-              <p className='text-gray-600 mt-4'>
-                Specification: Best Quality Graded, Round Shaped
-                <br />
-                Ready to use, No Wastage
-                <br />
-                Size: 19mm+
-                <br />
-                Mandi: 6 sutta
-                <br />
-                Bag Size: 5kg/8kg/10kg
-              </p>
+            <div className='bg-white rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 transition-transform'>
+              <picture>
+                <source srcSet='/gradeB.avif?webp' type='image/webp' />
+                <Image
+                  src='/gradeB.avif'
+                  alt='Grade B Foxnuts'
+                  width={120}
+                  height={200}
+                  className='w-full h-48 object-cover'
+                />
+              </picture>
+              <div className='p-6'>
+                <h2
+                  className='text-2xl font-bold text-gray-700 mb-4'
+                  style={{ fontFamily: inter.style.fontFamily }}
+                >
+                  Grade B
+                </h2>
+                <p className='text-gray-600'>
+                  Perfect for roasted or flavored foxnut production.
+                </p>
+                <p className='text-gray-600 mt-4'>
+                  Specification: Best Quality Graded, Round Shaped
+                  <br />
+                  Ready to use, No Wastage
+                  <br />
+                  Size: 16mm -19 mm
+                  <br />
+                  Mandi: 5 sutta
+                  <br />
+                  Bag Size: 5kg/8kg/10kg
+                </p>
+              </div>
             </div>
-          </div>
-          {/* </div>
+
+            <div className='bg-white rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 transition-transform'>
+              <picture>
+                <source srcSet='/gradeC.avif?webp' type='image/webp' />
+                <Image
+                  src='/gradeC.avif'
+                  alt='Grade C Foxnuts'
+                  width={120}
+                  height={200}
+                  className='w-full h-48 object-cover'
+                />
+              </picture>
+              <div className='p-6'>
+                <h2
+                  className='text-2xl font-bold text-gray-700 mb-4'
+                  style={{ fontFamily: inter.style.fontFamily }}
+                >
+                  Grade C
+                </h2>
+                <p className='text-gray-600'>
+                  Used in culinary recipes like curries, gravies, and snacks.
+                </p>
+                <p className='text-gray-600 mt-4'>
+                  Specification: Best Quality Graded, Round Shaped
+                  <br />
+                  Ready to use, No Wastage
+                  <br />
+                  Size: 19mm+
+                  <br />
+                  Mandi: 6 sutta
+                  <br />
+                  Bag Size: 5kg/8kg/10kg
+                </p>
+              </div>
+            </div>
+            {/* </div>
 
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto mt-8'>
           Row 2 - 3 Cards */}
-          <div className='bg-white rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 transition-transform'>
-            <picture>
-              <source srcSet='/smallPackaged.avif?webp' type='image/webp' />
-              <Image
-                src='/smallPackaged.avif'
-                alt='Packaged Raw Makhana - Small'
-                width={120}
-                height={200}
-                className='w-full h-48 object-cover'
-              />
-            </picture>
-            <div className='p-6'>
-              <h2
-                className='text-2xl font-bold text-gray-700 mb-4'
-                style={{ fontFamily: inter.style.fontFamily }}
-              >
-                Packaged Raw Makhana - Small
-              </h2>
-              <p className='text-gray-600'>
-                Small-sized packaged foxnuts for convenience stores.
-              </p>
-              <p className='text-gray-600 mt-4'>
-                Specification: Best Quality Graded, Round Shaped
-                <br />
-                Ready to use, No Wastage
-                <br />
-                Size: 17-19 mm
-                <br />
-                Package Size: 200g
-              </p>
+            <div className='bg-white rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 transition-transform'>
+              <picture>
+                <source srcSet='/smallPackaged.avif?webp' type='image/webp' />
+                <Image
+                  src='/smallPackaged.avif'
+                  alt='Packaged Raw Makhana - Small'
+                  width={120}
+                  height={200}
+                  className='w-full h-48 object-cover'
+                />
+              </picture>
+              <div className='p-6'>
+                <h2
+                  className='text-2xl font-bold text-gray-700 mb-4'
+                  style={{ fontFamily: inter.style.fontFamily }}
+                >
+                  Packaged Raw Makhana - Small
+                </h2>
+                <p className='text-gray-600'>
+                  Small-sized packaged foxnuts for convenience stores.
+                </p>
+                <p className='text-gray-600 mt-4'>
+                  Specification: Best Quality Graded, Round Shaped
+                  <br />
+                  Ready to use, No Wastage
+                  <br />
+                  Size: 17-19 mm
+                  <br />
+                  Package Size: 200g
+                </p>
+              </div>
             </div>
-          </div>
 
-          <div className='bg-white rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 transition-transform'>
-            <picture>
-              <source srcSet='/largePackaged.avif?webp' type='image/webp' />
-              <Image
-                src='/largePackaged.avif'
-                alt='Packaged Raw Makhana - Large'
-                width={120}
-                height={200}
-                className='w-full h-48 object-cover'
-              />
-            </picture>
-            <div className='p-6'>
-              <h2
-                className='text-2xl font-bold text-gray-700 mb-4'
-                style={{ fontFamily: inter.style.fontFamily }}
-              >
-                Packaged Raw Makhana - Large
-              </h2>
-              <p className='text-gray-600'>
-                Bulk packaged foxnuts for large-scale retail.
-              </p>
-              <p className='text-gray-600 mt-4'>
-                Specification: Best Quality Graded, Round Shaped
-                <br />
-                Ready to use, No Wastage
-                <br />
-                Size: 17-19mm
-                <br />
-                Package Size: 500g
-              </p>
+            <div className='bg-white rounded-lg shadow-lg overflow-hidden transform hover:-translate-y-2 transition-transform'>
+              <picture>
+                <source srcSet='/largePackaged.avif?webp' type='image/webp' />
+                <Image
+                  src='/largePackaged.avif'
+                  alt='Packaged Raw Makhana - Large'
+                  width={120}
+                  height={200}
+                  className='w-full h-48 object-cover'
+                />
+              </picture>
+              <div className='p-6'>
+                <h2
+                  className='text-2xl font-bold text-gray-700 mb-4'
+                  style={{ fontFamily: inter.style.fontFamily }}
+                >
+                  Packaged Raw Makhana - Large
+                </h2>
+                <p className='text-gray-600'>
+                  Bulk packaged foxnuts for large-scale retail.
+                </p>
+                <p className='text-gray-600 mt-4'>
+                  Specification: Best Quality Graded, Round Shaped
+                  <br />
+                  Ready to use, No Wastage
+                  <br />
+                  Size: 17-19mm
+                  <br />
+                  Package Size: 500g
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </>
   )
 }
 
