d62f52c297b574f9c2df34dc424f7c94e0d658bb
Nitesh Gupta <niteskug@Niteshs-MacBook-Pro.local>
2024-12-29 19:07:56 +0530

    added nutritional value page as svg

--- src/app/healthBenefits/page.tsx
+++ src/app/healthBenefits/page.tsx
@@ -4,6 +4,16 @@ const FoxnutBenefits: React.FC = () => {
   return (
     <section className='bg-gradient-to-b from-blue-50 to-blue-100  '>
       <picture>
+        <source srcSet={'/nutrition.svg'} type='image/webp' />
+        <Image
+          src={'/nutrition.svg'}
+          alt={'Nutriotional value of Makhana'}
+          width={1200}
+          height={1200}
+          className='w-full object-contain'
+        />
+      </picture>
+      <picture>
         <source srcSet={'/benefits.svg'} type='image/webp' />
         <Image
           src={'/benefits.svg'}
