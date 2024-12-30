a16fba70681dcfb3102c23a89fef89f4fa9b4244
Nitesh Gupta <niteskug@Niteshs-MacBook-Pro.local>
2024-12-30 14:55:50 +0530

    type error fix

--- src/app/components/pdfview.tsx
+++ src/app/components/pdfview.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-const PdfViewer = ({ pdfUrl }) => {
+// Define the type for props
+interface PdfViewerProps {
+  pdfUrl: string
+}
+
+const PdfViewer: React.FC<PdfViewerProps> = ({ pdfUrl }) => {
   return (
     <>
       <div className='w-full h-screen flex justify-center items-center overflow-hidden bg-gray-100'>
-        <div className='w-[100vh] h-[90vh]  md:w-[90-vh] border-4 border-white  shadow-lg'>
+        <div className='w-[100vh] h-[90vh] md:w-[90-vh] border-4 border-white shadow-lg'>
           <iframe
             src={pdfUrl}
             className='border-none w-full h-full'
