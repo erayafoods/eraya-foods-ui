86152eb10e9aa9beea1362986b68d1fa43c09c22
Nitesh Gupta <niteskug@Niteshs-MacBook-Pro.local>
2025-01-18 16:47:55 +0530

    updated security

--- next.config.mjs
+++ next.config.mjs
@@ -1,57 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// import withOptimizedImages from 'next-optimized-images'
-
-// const nextConfig = withOptimizedImages({
-//   webpack (config) {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       use: ['@svgr/webpack']
-//     })
-
-//     return config
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true
-//   },
-//   compiler: {
-//     removeConsole: true
-//   },
-//   async headers () {
-//     return [
-//       {
-//         source: '/(.*)',
-//         headers: [
-//           {
-//             key: 'X-Frame-Options',
-//             value: 'SAMEORIGIN' // Ensures the app can embed only safe origins
-//           },
-//           {
-//             key: 'Referrer-Policy',
-//             value: 'strict-origin-when-cross-origin'
-//           },
-//           {
-//             key: 'X-Content-Type-Options',
-//             value: 'nosniff'
-//           },
-//           {
-//             key: 'Content-Security-Policy',
-//             value:
-//               "default-src 'self'; " +
-//               "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://unpkg.com https://www.google-analytics.com; " +
-//               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
-//               "img-src 'self' data: https://drive.google.com https://maps.googleapis.com; " +
-//               "font-src 'self' https://fonts.gstatic.com; " +
-//               "connect-src 'self' https://www.google-analytics.com; " +
-//               "frame-src 'self' https://www.google.com https://maps.google.com https://maps.googleapis.com;"
-//           }
-//         ]
-//       }
-//     ]
-//   }
-// })
-
-// export default nextConfig
-
 /** @type {import('next').NextConfig} */
 import withOptimizedImages from 'next-optimized-images'
 
@@ -61,6 +7,7 @@ const nextConfig = withOptimizedImages({
       test: /\.svg$/,
       use: ['@svgr/webpack']
     })
+
     return config
   },
   eslint: {
@@ -76,7 +23,7 @@ const nextConfig = withOptimizedImages({
         headers: [
           {
             key: 'X-Frame-Options',
-            value: 'SAMEORIGIN' // Ensures the app can embed only safe origins
+            value: 'ALLOW-FROM https://drive.google.com'
           },
           {
             key: 'Referrer-Policy',
@@ -88,15 +35,8 @@ const nextConfig = withOptimizedImages({
           },
           {
             key: 'Content-Security-Policy',
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://unpkg.com https://www.google-analytics.com",
-              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-              "img-src 'self' data: https://*.googleusercontent.com https://maps.googleapis.com",
-              "font-src 'self' https://fonts.gstatic.com",
-              "connect-src 'self' https://www.google-analytics.com",
-              "frame-src 'self' https://www.google.com https://maps.google.com https://maps.googleapis.com"
-            ].join('; ')
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://unpkg.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https://drive.google.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://www.google-analytics.com; frame-src 'self' https://drive.google.com frame-src 'self' https://www.google.com/maps"
           }
         ]
       }
