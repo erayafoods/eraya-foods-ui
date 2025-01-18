336c0df54d9fd66cefbdfa472c5f1f1416cc5146
Nitesh Gupta <niteskug@Niteshs-MacBook-Pro.local>
2025-01-18 16:56:29 +0530

    sec

--- next.config.mjs
+++ next.config.mjs
@@ -36,8 +36,15 @@ const nextConfig = withOptimizedImages({
           },
           {
             key: 'Content-Security-Policy',
-            value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://unpkg.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https://drive.google.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://www.google-analytics.com; frame-src 'self' https://drive.google.com  https://www.google.com/maps"
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com https://*.gstatic.com https://*.google.com https://*.googleusercontent.com;
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+              img-src 'self' data: https://*.googleapis.com https://*.gstatic.com https://*.google.com https://*.googleusercontent.com;
+              font-src 'self' https://fonts.gstatic.com;
+              connect-src 'self' https://*.googleapis.com https://*.google.com https://*.gstatic.com;
+              frame-src 'self' https://*.google.com;
+            `
           }
         ]
       }
