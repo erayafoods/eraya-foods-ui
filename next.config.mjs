f9fed13be5d4b449a0f79e7d3f3c49740e6836c3
Nitesh Gupta <niteskug@Niteshs-MacBook-Pro.local>
2025-01-18 17:18:32 +0530

    map

--- next.config.mjs
+++ next.config.mjs
@@ -36,15 +36,14 @@ const nextConfig = withOptimizedImages({
           },
           {
             key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com https://*.gstatic.com https://*.google.com https://*.googleusercontent.com;
-              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-              img-src 'self' data: https://*.googleapis.com https://*.gstatic.com https://*.google.com https://*.googleusercontent.com;
-              font-src 'self' https://fonts.gstatic.com;
-              connect-src 'self' https://*.googleapis.com https://*.google.com https://*.gstatic.com;
-              frame-src 'self' https://*.google.com;
-            `
+            value:
+              "default-src 'self'; " +
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com https://*.gstatic.com https://*.google.com https://*.googleusercontent.com; " +
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+              "img-src 'self' data: https://*.googleapis.com https://*.gstatic.com https://*.google.com https://*.googleusercontent.com; " +
+              "font-src 'self' https://fonts.gstatic.com; " +
+              "connect-src 'self' https://*.googleapis.com https://*.google.com https://*.gstatic.com; " +
+              "frame-src 'self' https://*.google.com;"
           }
         ]
       }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+
 export const metadata: Metadata = {
   title: 'Get in Touch | Eraya Foods – Premium Phool Makhana, Foxnuts',
   description:
@@ -30,11 +31,11 @@ export default function Contact () {
   return (
     <>
       <ContactUs />
-      <div className='flex justify-center items-center my-8'>
+      <div className='flex justify-around items-center my-8'>
         <iframe
           title='Google Map of Eraya Foods Location'
           src='https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d218.95432852576604!2d77.09321853443376!3d28.59169816259517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sgali%20number%2031%2C%20near%20pradhan%20chowk%2C%20palam%2C%20new%20delhi.%20pin%20code%3A%20110045!5e0!3m2!1sen!2sin!4v1736840060191!5m2!1sen!2sin'
-          className='w-full max-w-[600px] aspect-video border-0'
+          className='w-full max-w-[600px] aspect-video border-2 border-gray-300 shadow-lg rounded-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:scale-105'
           allowFullScreen
           loading='lazy'
           referrerPolicy='no-referrer-when-downgrade'
