c53b2107c94fc683edf229ae502f6e8dfc8ee128
Nitesh Gupta <niteskug@Niteshs-MacBook-Pro.local>
2025-02-16 14:48:34 +0530

    product section color

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -47,54 +47,54 @@ const ibm = IBM_Plex_Serif({
 
 const products = [
   {
-    image: '/1_sutta.avif',
-    alt: '1 Sutta Foxnuts',
-    title: '1 Sutta',
-    description: 'Top-quality foxnuts, ideal for premium snack packaging.',
-    specifications: `Specification: Best Quality Graded, Round Shaped\nSize: Approximately 3.17 mm in diameter.`
-  },
-  {
-    image: '/2_sutta.avif',
-    alt: '2 Sutta Foxnuts',
-    title: '2 Sutta',
-    description: 'Perfect for roasted or flavored foxnut production.',
-    specifications: `Specification: Best Quality Graded, Round Shaped\nSize:  Approximately 6.34 mm in diameter.`
-  },
-  {
-    image: '/3_sutta.avif',
-    alt: '3 Sutta Foxnuts',
-    title: '3 Sutta',
-    description: 'Used in culinary recipes like curries, gravies, and snacks.',
-    specifications: `Specification: Best Quality Graded, Round Shaped\nSize: Approximately 9.51 mm in diameter.`
-  },
-  {
-    image: '/4_sutta.avif',
-    alt: '4 Sutta Foxnuts',
-    title: '4 Sutta',
-    description: 'Best for bulk usage in food manufacturing and exports.',
-    specifications: `Specification: Premium Quality, Slight Oval Shaped\nSize:  Approximately 12.68 mm in diameter.`
-  },
-  {
     image: '/5_sutta.avif',
     alt: '5 Sutta Foxnuts',
-    title: '5 Sutta',
+    title: '5 Sutta Makhana',
     description: 'Ideal for health food brands focusing on high-grade quality.',
     specifications: `Specification: High Grade, Evenly Sized\nSize: Approximately 15.85 mm in diameter`
   },
   {
     image: '/6_sutta.avif',
     alt: '6 Sutta Foxnuts',
-    title: '6 Sutta',
+    title: '6 Sutta Makhana',
     description: 'Preferred by gourmet chefs for unique culinary applications.',
     specifications: `Specification: High Quality, Larger Foxnuts\nSize: Approximately 19.02 mm in diameter.`
   },
   {
+    image: '/4_sutta.avif',
+    alt: '4 Sutta Foxnuts',
+    title: '4 Sutta Makhana',
+    description: 'Best for bulk usage in food manufacturing and exports.',
+    specifications: `Specification: Premium Quality, Slight Oval Shaped\nSize:  Approximately 12.68 mm in diameter.`
+  },
+  {
     image: '/gitanjali.avif',
     alt: 'Gitanjali Mix Foxnuts',
-    title: 'Gitanjali Mix',
+    title: 'Gitanjali Mix Makhana',
     description:
       'A premium mix, perfect for diverse culinary and packaging needs.',
     specifications: `Specification: Mixed Quality Graded, Multiple Sizes\nSize: Mix of 4,5+ Sutta`
+  },
+  {
+    image: '/1_sutta.avif',
+    alt: '1 Sutta Foxnuts',
+    title: '1 Sutta Makhana',
+    description: 'Top-quality foxnuts, ideal for premium snack packaging.',
+    specifications: `Specification: Best Quality Graded, Round Shaped\nSize: Approximately 3.17 mm in diameter.`
+  },
+  {
+    image: '/2_sutta.avif',
+    alt: '2 Sutta Foxnuts',
+    title: '2 Sutta Makhana',
+    description: 'Perfect for roasted or flavored foxnut production.',
+    specifications: `Specification: Best Quality Graded, Round Shaped\nSize:  Approximately 6.34 mm in diameter.`
+  },
+  {
+    image: '/3_sutta.avif',
+    alt: '3 Sutta Foxnuts',
+    title: '3 Sutta Makhana',
+    description: 'Used in culinary recipes like curries, gravies, and snacks.',
+    specifications: `Specification: Best Quality Graded, Round Shaped\nSize: Approximately 9.51 mm in diameter.`
   },
   {
     image: '/customized.avif',
@@ -152,36 +152,37 @@ const Products = () => {
       <div className='content-center px-[2vh] md:px-[8vh] pb-[6vh] bg-gray-100'>
         <section className='py-16'>
           <h1
-            className='text-4xl text-center font-extrabold text-gray-800 mb-12'
+            className='text-2xl text-center font-extrabold text-gray-800 mb-12'
             style={{ fontFamily: ibm.style.fontFamily }}
           >
-            Products Catalogue
+            Explore Our Products
           </h1>
 
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:px-[6vw]'>
             {products.map((product, index) => (
-              <div
-                key={index}
-                className='bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-all duration-300 transform hover:scale-105 hover:bg-gray-50 p-6'
-              >
-                <picture>
-                  <source srcSet={`${product.image}?webp`} type='image/webp' />
-                  <Image
-                    src={product.image}
-                    alt={product.alt}
-                    width={100}
-                    height={100}
-                    className='w-full h-56 object-contain rounded-lg mb-4'
-                  />
-                </picture>
-                <div className='p-4'>
-                  <h2 className='text-2xl font-bold text-gray-700 mb-4'>
-                    {product.title}
-                  </h2>
-                  <p className='text-gray-600 mb-4'>{product.description}</p>
-                  <p className='text-gray-600  mt-2 whitespace-pre-line'>
-                    {product.specifications}
-                  </p>
+              <div>
+                <div
+                  key={index}
+                  className='bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-all duration-300 transform hover:scale-105 hover:bg-gray-50 p-6'
+                >
+                  <picture>
+                    <source
+                      srcSet={`${product.image}?webp`}
+                      type='image/webp'
+                    />
+                    <Image
+                      src={product.image}
+                      alt={product.alt}
+                      width={100}
+                      height={100}
+                      className='w-full h-56 object-contain rounded-lg mb-4'
+                    />
+                  </picture>
+                  <div className='p-4 bg-[#964663]'>
+                    <h2 className='text-xl font-bold text-white mb-4 '>
+                      {product.title}
+                    </h2>
+                  </div>
                 </div>
               </div>
             ))}
